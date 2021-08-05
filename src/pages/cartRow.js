bee01fcdb2805d3e2cf6a24f09f9d6f206e3ff75
sachie026 <jadhavsachin174@gmail.com>
2021-08-05 16:34:00 +0530

    Add cart row styling

--- src/pages/cartRow.js
+++ src/pages/cartRow.js
@@ -8,23 +8,29 @@ const CartRow = (props) => {
   const { item, incrementAction, decrementAction, deleteAction } = props;
   return (
     <div key={`item-${item.id}`} className="cart-row">
-      <h3>{item.name}</h3> {item.quantity}
-      <br />
-      {item.total}
-      <br />
-      <button onClick={() => incrementAction(item.id, item.quantity)}>
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-      <button
-        disabled={item.quantity === 0}
-        onClick={() => decrementAction(item.id, item.quantity)}
-      >
-        <FontAwesomeIcon icon={faMinus} />
-      </button>
-      <button onClick={() => deleteAction(item.id)}>
-        <FontAwesomeIcon icon={faTimes} />
-      </button>
-      <br />
+      <div className="item-name">
+        <label>{item.name}</label>
+        <label>{item.id}</label>
+      </div>
+      <div className="item-quantity-container">
+        <button
+          disabled={item.quantity === 0}
+          onClick={() => decrementAction(item.id, item.quantity)}
+        >
+          <FontAwesomeIcon icon={faMinus} size="6px"/>
+        </button>
+        <div className="item-quantity">{item.quantity}</div>
+        <button onClick={() => incrementAction(item.id, item.quantity)}>
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+      <div className="item-total">{item.total}</div>
+
+      <div className="item-remove">
+        <button onClick={() => deleteAction(item.id)}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
     </div>
   );
 };
